8e3e139504d814f4e198d63e27b9a16a407bd79e
adam-watkins <cadamrun@gmail.com>
2020-02-21 02:56:08 -0800

    basic layout

---
+++ components/footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-footer>
+    <v-col>
+      <v-row justify="center">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon.icon"
+          :href="icon.link"
+          target="_blank"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon :color="iconColor" size="24px">
+            {{ icon.icon }}
+          </v-icon>
+        </v-btn>
+      </v-row>
+      <v-row justify="center">
+        {{ new Date().getFullYear() }} - Adam Watkins
+      </v-row>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+import { mdiGithubCircle, mdiInstagram, mdiLinkedin, mdiEmail } from '@mdi/js'
+
+export default {
+  name: 'Footer',
+  data: () => ({
+    icons: [
+      {
+        icon: mdiEmail,
+        link: 'mailto:adam@wtkns.dev'
+      },
+      {
+        icon: mdiGithubCircle,
+        link: 'https://github.com/adam-watkins'
+      },
+      {
+        icon: mdiLinkedin,
+        link: 'https://www.linkedin.com/in/adam-watkins-yvr/'
+      },
+      {
+        icon: mdiInstagram,
+        link: 'https://www.instagram.com/'
+      }
+    ]
+  }),
+  computed: {
+    iconColor() {
+      if (this.$vuetify.theme.isDark) return 'white'
+      else return 'black'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
