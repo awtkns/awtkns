429a817d8f4756f5cc5e46862f5820da1b37f580
adam-watkins <cadamrun@gmail.com>
2020-02-25 21:50:34 -0800

    Timeline component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <v-parallax src="hero.jpg" />
+    <v-sheet color="warning" tile>
+      <v-card-title>
+        My website is under development.
+      </v-card-title>
+      <v-card-subtitle>
+        You've been warned.
+      </v-card-subtitle>
+    </v-sheet>
     <v-container fluid>
       <v-col>
         <About :class="listMargin" />
         <Blog :class="listMargin" />
+        <h1 style="text-align: center">Experience</h1>
         <Work :class="listMargin" />
         <Projects :class="listMargin" />
         <Skills :class="listMargin" />
