7c046d7e166bf65e304c5a172130f8a95b9f5aa1
adam-watkins <cadamrun@gmail.com>
2020-02-26 01:51:13 -0800

    Project view pulled directly from git. Proper Resizing

--- components/projects.vue
+++ components/projects.vue
@@ -1,11 +1,83 @@
 <template>
-  <v-sheet elevation="24">
-    <v-card-title><h1>Projects</h1></v-card-title>
-  </v-sheet>
+  <div>
+    <v-row>
+      <v-col>
+        <h1 style="text-align: center">Technical Projects</h1>
+        <h5 style="text-align: center">
+          Straight From Github
+        </h5>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="project in projects"
+        :key="project.id"
+        cols="12"
+        md="4"
+        lg="3"
+      >
+        <v-card :href="project.html_url" target="_blank">
+          <v-card-title class="primary--text" v-text="project.name" />
+          <v-card-subtitle v-text="getDate(project)" />
+          <v-card-text v-text="project.description" />
+          <v-card-actions><v-spacer />{{ project.language }}</v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h5 style="text-align: center">
+          Contributions Over the Past Year
+        </h5>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card elevation="24">
+          <v-card-text>
+            <v-img :src="githubChartURL" alt="adam-watkins's Github chart" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  data() {
+    return {
+      projects: []
+    }
+  },
+  computed: {
+    githubChartURL() {
+      const hex = this.$vuetify.theme.parsedTheme.primary.base.substr(1)
+
+      return `http://ghchart.rshah.org/${hex}/adam-watkins`
+    }
+  },
+  async created() {
+    const result = await this.$axios.get(
+      'https://api.github.com/users/adam-watkins/repos'
+    )
+    this.projects = result.data
+  },
+  methods: {
+    getDate(project) {
+      let date = new Date(project.created_at)
+      let year = date.getFullYear()
+      let month = date.toLocaleString('default', { month: 'short' })
+      const start = `${month} ${year}`
+
+      date = new Date(project.updated_at)
+      year = date.getFullYear()
+      month = date.toLocaleString('default', { month: 'short' })
+      const end = `${month} ${year}`
+
+      return start === end ? start : `${start} - ${end}`
+    }
+  }
 }
 </script>
--- components/work.vue
+++ components/work.vue
@@ -1,21 +1,33 @@
 <template>
-  <v-timeline>
-    <v-timeline-item
-      v-for="item in items"
-      :key="item"
-      :icon="item.timelineIcon"
-    >
-      <span slot="opposite">{{ item.date }}</span>
-      <v-card>
-        <v-card-title>
-          {{ item.title }}
-          <v-spacer />
-          <v-icon v-for="icon in item.icons" :key="icon" v-text="icon" />
-        </v-card-title>
-        <v-card-subtitle v-text="item.subtitle" />
-      </v-card>
-    </v-timeline-item>
-  </v-timeline>
+  <div>
+    <v-row>
+      <v-col>
+        <h1 style="text-align: center">Experience</h1>
+        <h5 style="text-align: center">
+          Professional and Educational
+        </h5>
+      </v-col>
+    </v-row>
+    <v-timeline :dense="isDense">
+      <v-timeline-item
+        v-for="item in items"
+        :key="item.name"
+        :icon="item.timelineIcon"
+        :color="item.color"
+      >
+        <span slot="opposite">{{ item.date }}</span>
+        <v-card>
+          <v-card-title>
+            {{ item.title }}
+            <v-spacer />
+            <v-icon v-for="icon in item.icons" :key="icon" v-text="icon" />
+          </v-card-title>
+          <v-card-subtitle v-text="item.subtitle" />
+          <v-card-text v-if="isDense" v-text="item.date" />
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
 </template>
 
 <script>
@@ -40,6 +52,8 @@ export default {
           title: 'Full Stack Developer',
           subtitle: 'BC Cancer Research Center',
           date: 'Jan 2020 - Present',
+          elevation: 10,
+          color: 'purple',
           timelineIcon: mdiBriefcase,
           icons: [
             mdiLanguagePython,
@@ -51,10 +65,19 @@ export default {
           ]
         },
         {
+          title: 'Simon Fraser University',
+          subtitle: 'Faculty Of Applied Science, Computing Science',
+          date: 'May 2018 - Present',
+          elevation: 10,
+          color: 'red',
+          timelineIcon: mdiSchool,
+          icons: []
+        },
+        {
           title: 'Bridging Languages Coding Contest',
           subtitle: 'Top 5 Team',
           date: 'Jan 2020',
-          timelineIcon: mdiSchool,
+          timelineIcon: mdiTrophyAward,
           icons: []
         },
         {
@@ -68,6 +91,7 @@ export default {
           title: 'Manager',
           subtitle: 'Reel Mac and Cheese',
           date: 'Aug 2014 - Dec 2019',
+          color: 'yellow',
           timelineIcon: mdiBriefcase,
           icons: []
         },
@@ -75,10 +99,24 @@ export default {
           title: 'Halite III AI Coding Contest',
           subtitle: 'Top 10 Canadian University Student',
           date: 'Oct 2018 - Jan 2019',
+          elevation: 10,
           timelineIcon: mdiTrophyAward,
           icons: [mdiLanguagePython]
+        },
+        {
+          title: 'Langara College',
+          subtitle: 'Computer Science',
+          date: 'Sep 2015 - Apr 2018',
+          color: 'orange',
+          timelineIcon: mdiSchool,
+          icons: []
         }
       ]
+    }
+  },
+  computed: {
+    isDense() {
+      return this.$vuetify.breakpoint.smAndDown
     }
   }
 }
